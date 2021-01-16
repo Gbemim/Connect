1cfa3ab8b088521383e545469d3ef47f9e4f71da
Gbemim <gbemisolaakerele@gmail.com>
2021-01-16 17:07:52 -0500

    redirect to no access if there was no login

--- docs/filter/filter.js
+++ docs/filter/filter.js
@@ -1,7 +1,8 @@
 function checkurl() {
     var thecurrenturl = window.location.href;
-    if (thecurrenturl == "https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_authorize&error_description=The+user+cancelled+the+authorization") {
+    // if (thecurrenturl == "https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_authorize&error_description=The+user+cancelled+the+authorization") {
         // Simulate an HTTP redirect:
-        window.location.replace("https://gbemim.github.io/Connect/filter/noaccess.html");
-    }
+        console.log("it worked")
+        // window.location.replace("https://gbemim.github.io/Connect/filter/noaccess.html");
+    // }
 }
