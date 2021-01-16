37a7fe96ed7d727980d75dc17319881fdfe80771
Gbemim <gbemisolaakerele@gmail.com>
2021-01-16 17:23:58 -0500

    redirect to no access if there was no login

--- docs/filter/filter.js
+++ docs/filter/filter.js
@@ -1,4 +1,4 @@
-function checkurl() {
+setTimeout(function checkurl() {
     var thecurrenturl = window.location.href;
     if (thecurrenturl == 'https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_authorize&error_description=The+user+cancelled+the+authorization' || 'https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_login&error_description=The+user+cancelled+LinkedIn+login') {
 
@@ -6,4 +6,4 @@ function checkurl() {
         console.log(thecurrenturl)
         window.location.href = 'https://gbemim.github.io/Connect/filter/noaccess.html';
     }
-}
+}, 8000);
