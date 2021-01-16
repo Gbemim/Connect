40f874a83572d550710435179be5d77c8268dd0d
Gbemim <gbemisolaakerele@gmail.com>
2021-01-16 17:32:18 -0500

    redirect to no access if there was no login

--- docs/filter/filter.js
+++ docs/filter/filter.js
@@ -1,9 +1,9 @@
-setTimeout(function checkurl() {
-    var thecurrenturl = window.location.href;
-    if (thecurrenturl == 'https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_authorize&error_description=The+user+cancelled+the+authorization' || 'https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_login&error_description=The+user+cancelled+LinkedIn+login') {
+// setTimeout(function checkurl() {
+//     var thecurrenturl = window.location.href;
+//     if (thecurrenturl == 'https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_authorize&error_description=The+user+cancelled+the+authorization' || 'https://gbemim.github.io/Connect/filter/filter.html?error=user_cancelled_login&error_description=The+user+cancelled+LinkedIn+login') {
 
-    // Simulate an HTTP redirect:
-        console.log(thecurrenturl)
-        window.location.href = 'https://gbemim.github.io/Connect/filter/noaccess.html';
-    }
-}, 8000);
+//     // Simulate an HTTP redirect:
+//         console.log(thecurrenturl)
+//         window.location.href = 'https://gbemim.github.io/Connect/filter/noaccess.html';
+//     }
+// }, 8000);
